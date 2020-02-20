bc3cc399bda3bca855e26a56a0eecf12b2b22a91
xopek55 <xopek55@gmail.com>
2020-02-20 15:55:43 +0300

    added products

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
 $(function(){
+        var mixer = mixitup('.products__main__inner');
+
+        $('.slider__inner-weekly-main').slick({
+   
+        slidesToShow: 2,
+                prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/icons/slider-left.png" alt=""></button>',
+                nextArrow: '<button class="slick-arrow slick-next"><img src="/images/icons/slider-right.png" alt=""></button>',
+        slidesToScroll: 2,
+        
+       
 
 
-var mixer = mixitup('.products__inner-box');
+
+        });
+     
+
+
+
 });
+
