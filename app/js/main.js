b5ee306a05a276feb35fdb894217fe62b8c1c9fb
xopek55 <xopek55@gmail.com>
2020-02-22 01:00:17 +0300

    edit site

--- app/js/main.js
+++ app/js/main.js
@@ -14,7 +14,7 @@ $(function(){
 
         });
      
-
+        $('input, select').styler();
 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,9 @@ gulp.task('style', function(){
         return gulp.src([
         'node_modules/normalize.css/normalize.css', 
         'node_modules/slick-carousel/slick/slick.css', 
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css'
 
         ])
         .pipe(concat('libs.min.css'))
@@ -37,7 +39,8 @@ gulp.task('script', function(){
         
         'node_modules/slick-carousel/slick/slick.js', 
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js'
 
         ])
         .pipe(concat('libs.min.js'))
