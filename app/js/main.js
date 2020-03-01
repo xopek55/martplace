4ca63de6798e9c042f745f9f09f31aaa9b6667a1
xopek55 <xopek55@gmail.com>
2020-03-01 18:53:58 +0300

    beta 1.0

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,25 @@
 $(function(){
         var mixer = mixitup('.products__main__inner');
 
-        $('.slider__inner-weekly-main').slick({
-   
-        slidesToShow: 2,
+                $('.slider__inner-weekly-main').slick({
+        
+                
+                        prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/icons/slider-left.png" alt=""></button>',
+                        nextArrow: '<button class="slick-arrow slick-next"><img src="/images/icons/slider-right.png" alt=""></button>',
+                
+                        
+
+                });
+
+        $('.slider__inner-followers-main').slick({
                 prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/icons/slider-left.png" alt=""></button>',
                 nextArrow: '<button class="slick-arrow slick-next"><img src="/images/icons/slider-right.png" alt=""></button>',
-        slidesToScroll: 2,
-        
-       
-
-
-
+                infinite: true,
+                slidesToShow:3,
+                slidesToScroll: 3,
+                // variableWidth: true
         });
+
      
         $('input, select').styler();
 
