516214c64bfc700cfd2da8758b2b0ab1f529e6f4
xopek55 <xopek55@gmail.com>
2020-03-16 17:18:48 +0300

    added page.html category.html(grid)  adaptive header\footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,39 @@
-$(function(){
-        var mixer = mixitup('.products__main__inner');
+$(function () {
 
-                $('.slider__inner-weekly-main').slick({
-        
-                
-                        prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/icons/slider-left.png" alt=""></button>',
-                        nextArrow: '<button class="slick-arrow slick-next"><img src="/images/icons/slider-right.png" alt=""></button>',
-                
-                        
+        $('.menu__btn').on('click', function () {
+                $('.menu__list').slideToggle();
+        });
 
-                });
+        $('.header__btn-menu').on('click', function () {
+                $('.header__top-info').toggleClass('active');
+        });
+
+
+
+
+        $('.slider__inner-weekly-main').slick({
+
+
+                prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/icons/slider-left.png" alt=""></button>',
+                nextArrow: '<button class="slick-arrow slick-next"><img src="/images/icons/slider-right.png" alt=""></button>',
+
+
+
+        });
 
         $('.slider__inner-followers-main').slick({
                 prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/icons/slider-left.png" alt=""></button>',
                 nextArrow: '<button class="slick-arrow slick-next"><img src="/images/icons/slider-right.png" alt=""></button>',
                 infinite: true,
-                slidesToShow:3,
+                slidesToShow: 3,
                 slidesToScroll: 3,
                 // variableWidth: true
         });
 
-     
+
         $('input, select').styler();
+
+
 
         $(".slider__weekly-main-cost-star").rateYo({
                 starWidth: "15px",
@@ -29,5 +41,84 @@ $(function(){
                 readOnly: true
         });
 
-});
+        // $(".js-range-slider").ionRangeSlider({
+        //         type: "double",
+        //         min: 0,
+        //         max: 1000,
+        //         from: 0,
+        //         to: 600,
+        //         prefix: "$",
+        //        
+        // });
 
+
+        var $range = $(".js-range-slider");
+        var $inputFrom = $(".range-slider-from");
+        var $inputTo = $(".range-slider-to");
+        var instance;
+        var min = 0;
+        var max = 1000;
+        var from = 0;
+        var to = 0;
+
+        $range.ionRangeSlider({
+                // skin: "round",
+                type: "double",
+                min: min,
+                max: max,
+                from: 200,
+                to: 800,
+                prefix: "$",
+                postfix: "$",
+                onStart: updateInputs,
+                onChange: updateInputs,
+                onFinish: updateInputs
+        });
+        instance = $range.data("ionRangeSlider");
+
+        function updateInputs(data) {
+                from = data.from;
+                to = data.to;
+
+                $inputFrom.prop("value", from);
+                $inputTo.prop("value", to);
+        }
+
+        $inputFrom.on("change", function () {
+                var val = $(this).prop("value");
+
+                // validate
+                if (val < min) {
+                        val = min;
+                } else if (val > to) {
+                        val = to;
+                }
+
+                instance.update({
+                        from: val
+                });
+
+                $(this).prop("value", val);
+
+        });
+
+        $inputTo.on("change", function () {
+                var val = $(this).prop("value");
+
+                // validate
+                if (val < from) {
+                        val = from;
+                } else if (val > max) {
+                        val = max;
+                }
+
+                instance.update({
+                        to: val
+                });
+
+                $(this).prop("value", val);
+        });
+
+
+        var mixer = mixitup('.products__main__inner');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task('style', function(){
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
-        'node_modules/rateyo/src/jquery.rateyo.css'
+        'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
 
         ])
         .pipe(concat('libs.min.css'))
@@ -42,7 +43,8 @@ gulp.task('script', function(){
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-        'node_modules/rateyo/src/jquery.rateyo.js'
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
 
         ])
         .pipe(concat('libs.min.js'))
